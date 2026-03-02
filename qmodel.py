ee32b51eab840887613579d78858938ab99c3d89
Daryll John <104271234+diem-john@users.noreply.github.com>
2026-03-02 02:17:00 +0000

    Added QModel D

--- qmodel.py
+++ qmodel.py
@@ -67,3 +67,24 @@ class QTransformerModel_ED(nn.Module):
         x = self.decoder(x[:, -1, :])
         x = self.post_net(x)
         return x
+
+class QTransformerModel_D(nn.Module):
+    def __init__(self, input_dim=5, d_model=128, nhead=4, num_layers=4, target_window=36, dropout=0.4):
+        super(QTransformerModel_ED, self).__init__()
+
+        self.encoder = ts.Linear(input_dim, d_model)
+        self.pos_encoder = ts.PositionalEncoding(d_model, dropout)
+        encoder_layers = ts.TransformerEncoderLayer(d_model, nhead)
+        self.transformer_encoder = qts.TransformerEncoder(encoder_layers, num_layers)
+        self.pre_net = ts.Linear(d_model, 128)
+        self.decoder = qts.QuantumDress()
+        self.post_net = ts.Linear(4, target_window)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.pos_encoder(x)
+        x = self.transformer_encoder(x)
+        x = self.pre_net(x)
+        x = self.decoder(x[:, -1, :])
+        x = self.post_net(x)
+        return x
